9fa127189bbd914b075a776fddbc894fac23f27e
pooria83 <pjalili83@gmail.com>
2020-02-01 20:55:54 +0330

    adding index file

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Water Pounding test</title>
+</head>
+<body>
+<form action="calc.php"  method="post">
+    <label for="firstVolume">First Container Volume : </label>
+    <input type="number" name="firstVolume">
+    <br><br>
+    <label for="secondVolume">Second Container Volume : </label>
+    <input type="number" name="secondVolume">
+    <br><br>
+    <label for="goal">goal : </label>
+    <input type="number" name="goal">
+    <br><br>
+    <input type="submit" name="submit" value="calculate">
+</form>
+
+</body>
+</html>
